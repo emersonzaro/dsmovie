47744f32dbb7f84f9268a552ac4cc94afcba57e7
Emerson Zaro <emersonzaro@gmail.com>
2022-01-11 10:59:14 -0300

    Project created

---
+++ backend/src/main/java/com/ezaro/demovei/DemoveiApplication.java
@@ -0,0 +1,13 @@
+package com.ezaro.demovei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoveiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoveiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ezaro/demovei/DemoveiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezaro.demovei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoveiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
